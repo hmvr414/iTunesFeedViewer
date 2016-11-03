baecce20d7c31dca66468c54e67474f5f9d139fe
Hermann Vallejo <hmvr414@gmail.com>
2016-11-02 20:51:11 -0500

    Initial Commit

---
+++ RappiChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RappiChallenge
+//
+//  Created by Hermann Vallejo on 2/11/16.
+//  Copyright © 2016 Hermann Vallejo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
